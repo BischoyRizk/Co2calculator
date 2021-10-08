a2b67e334ce8289a84fab1a402ae18a0102650f3
BischoyRizk <bischoy.rizk13@hotmail.com>
2021-10-08 19:59:00 +0200

    added variable

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,17 @@
 public class Calculator {
-    
+
     public static void main(String[] args) {
 
+        int a = 1;
+        double b = 3.8;
+        boolean c = true;
+        char d = 'g';
+        String e = "test";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
