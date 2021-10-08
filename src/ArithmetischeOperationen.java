e0edbc5cfb55c2e2526c9c61ca238d365c8c3601
BischoyRizk <bischoy.rizk13@hotmail.com>
2021-10-08 20:22:40 +0200

    mathematischeoperatoren

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,16 @@
+public class ArithmetischeOperationen {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        int multi = a*b;
+        int p = 2 + multi;
+        System.out.println(p);
+
+        System.out.println("Addition: " + (a+b));
+        System.out.println(a-b);
+        System.out.println(multi);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
