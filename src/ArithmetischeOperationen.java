47b063f03e979fd53d67ec267143cee7cd037cda
BischoyRizk <bischoy.rizk13@hotmail.com>
2021-10-08 21:29:40 +0200

    mathematischeoperatoren

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -3,14 +3,55 @@ public class ArithmetischeOperationen {
         int a = 8;
         int b = 5;
 
-        int multi = a*b;
+        int multi = a * b;
         int p = 2 + multi;
         System.out.println(p);
+        int divison = a / b;
+        int r = 4 * divison;
+        System.out.println(r);
 
-        System.out.println("Addition: " + (a+b));
-        System.out.println(a-b);
+        System.out.println("Addition: " + (a + b));
+        System.out.println(a - b);
         System.out.println(multi);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        System.out.println(divison);
+        System.out.println(a % b);
+
+        printResult(a+b);
+       int result = add(2, 5);
+       printResult(result);
+       printResult(add(2,5));
+
+       int sub = subb(6,4);
+        System.out.println(sub);
+        printResult(subb(4,7));
+        printResult(add(4,7));
+        printResult(div(4,7));
+        printResult(mult(4,7));
+
+    }
+
+    public static void printResult ( double result){
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+
+    }
+
+
+    public static int subb(int a, int b){
+
+        return a - b;
+    }
+
+    public static double div( double a, double b){
+
+        return a/b;
+
+    }
+
+    public static int mult(int a, int b){
+        return a*b;
     }
 }
