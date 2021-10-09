92ec2b2e18db6511ab98f80c89aced1392ec97f5
BischoyRizk <bischoy.rizk13@hotmail.com>
2021-10-09 11:06:58 +0200

    mathematischeoperatoren

---
+++ src/ForSchleife.java
@@ -0,0 +1,11 @@
+public class ForSchleife {
+    public static void main(String[] args) {
+        for(int i = 1; i <=10; i++){
+            System.out.println(i);
+
+        }
+            for (int j = 10; j >= 1; j--){
+                System.out.println(j);
+            }
+    }
+}
